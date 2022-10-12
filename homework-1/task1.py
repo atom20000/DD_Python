802e26c238a653c8a1534efd2f912e7ac8e0d6d3
atom20000 <0362725@mail.ru>
2022-10-12 18:52:27 +0300

    Add float working

--- homework-1/task1.py
+++ homework-1/task1.py
@@ -1,16 +1,16 @@
 
 def calculate():
     num1 = input('первое число\n')
-    if not num1.isdigit():
+    if not num1.isnumeric():
         print('Error')
         return
     if (oper := input('Оператор из списка: +, -, *, /, **, %, radical\n')) \
         and oper not in ('+', '-', '*', '/', '**', '%', 'radical'):
         print('Error')
     else:
-        if (num2 := input('второе число\n')) and num2.isdigit():
+        if (num2 := input('второе число\n')) and num2.isnumeric():
             if oper == 'radical' and int(num2) != 0:
-                print(f'{num2}\u221A{num1} = {int(num1) ** (1 / int(num2))}')
+                print(f'{num2}\u221A{num1} = {float(num1) ** (1 / float(num2))}')
             else:
                 if oper == '/' and int(num2) == 0:
                     print('Error')
