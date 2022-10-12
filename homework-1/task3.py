51aec32c6cc3d4e02eec28f28f3f41511b426812
atom20000 <0362725@mail.ru>
2022-10-13 00:26:09 +0300

    Finish task 3. We are the champions.

--- homework-1/task3.py
+++ homework-1/task3.py
@@ -4,6 +4,7 @@ from sys import stdin
 def distance(a, b): #Расстояние Левенштейна
     n, m = len(a), len(b)
     etal = b
+    error_pos = []
     if n > m:
         a, b = b, a
         n, m = m, n
@@ -14,24 +15,24 @@ def distance(a, b): #Расстояние Левенштейна
             add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
             if a[j - 1] != b[i - 1]:
                 change += 1
-            current_row[j] = min(add, delete, change)
-            print(current_row)
-    return (current_row[n], etal)
+                if j == i:
+                    error_pos.append(j-1)
+            min_act = min((add, 'add'), (delete, 'delete'), (change, 'change'))
+            current_row[j] = min_act[0]
+    return (current_row[n], error_pos, etal)
 
 def check_grammar():
     print('каждое слово с новой строки. Остановите ввод нажатие ctr+D')
     true_word = list(map(lambda x : x.rstrip(), stdin.readlines()))
     sentence = (input('Введите проверяемое предложение\n')).split()
-    #import pdb; pdb.set_trace()
     for i in set(true_word).intersection(sentence): sentence.remove(i)
     for word in sentence:
-        #for true_w in list(filter(lambda x: abs(len(x)-len(word)) < 3, true_word)):
+        check = []
         for true_w in true_word:
-            
-            pass
-
+            check.append(distance(word, true_w))
+        min_dis = min(check)
+        print(''.join([ f' {i} ' if j in min_dis[1] else i for i, j in zip(word, range(len(word)))]), f'\nЭталонное слова {min_dis[2]}')
 
 if __name__ == '__main__':
-    #check_grammar()
-    print(distance('мапа','мама'))
+    check_grammar()
     
