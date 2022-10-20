a556fc6fa3d4fdca9d22843f6e5bd5be043710d7
atom20000 <0362725@mail.ru>
2022-10-20 13:28:24 +0300

    Add number check

--- homework-2/task3.py
+++ homework-2/task3.py
@@ -1,7 +1,11 @@
 
 def sum_of_digits(num):
-    print(f'Сумма цифр чисал {num} равна', sum(map(int, list(num))))
+    if not num.isdigit():
+        print('Вы ввели не число')
+        return
+    return sum(map(int, list(num)))
 
 
 if __name__ == '__main__':
-    sum_of_digits(input('Введите число\n'))
+    num = input('Введите число\n')
+    print(f'Сумма цифр чисал {num} равна', sum_of_digits(num))
