fd3c93171d87e4edb552b2ced4a39aef0b5a3655
atom20000 <0362725@mail.ru>
2022-10-20 13:20:32 +0300

    PEP8 style

--- homework-2/task3.py
+++ homework-2/task3.py
@@ -1,6 +1,6 @@
 
 def sum_of_digits(num):
-    print(f'Сумма цифр чисал {num} равна', sum(map(int,list(num))))
+    print(f'Сумма цифр чисал {num} равна', sum(map(int, list(num))))
 
 
 if __name__ == '__main__':
